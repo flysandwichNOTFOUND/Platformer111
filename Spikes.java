66281d58d0417bb990b2a020166265c8e57ea0e4
flysandwichNOTFOUND <frankgao1145@gmail.com>
2025-09-20 15:41:34 -0700

    Update Spikes.java

--- Spikes.java
+++ Spikes.java
@@ -1,13 +1,31 @@
-public Spikes{
-    
-    Boolean touched;
-    public Spikes(int x, int y){
+public class Spikes {
+    private int x, y, width, height;
+    private boolean touched;
+
+    public Spikes(int x, int y, int width, int height) {
         this.x = x;
         this.y = y;
-        this.touched = False;
-        
-        }
-        
-    
-    
+        this.width = width;
+        this.height = height;
+        this.touched = false;
     }
+
+    public boolean spikeTouchesPlayer(Platform player) {
+        touched =
+            x < player.getX() + player.getWidth() &&
+            x + width > player.getX() &&
+            y < player.getY() + player.getHeight() &&
+            y + height > player.getY();
+        return touched;
+    }
+
+    public int getX() { 
+        return x;
+        }
+    public int getY() {
+        return y; 
+        }
+    public boolean isTouched() {
+        return touched; 
+        }
+}
