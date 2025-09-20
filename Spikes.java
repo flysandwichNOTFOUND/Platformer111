640c189175b8c5b5afb1188b1efc2ee5f1778b7f
flysandwichNOTFOUND <frankgao1145@gmail.com>
2025-09-20 14:56:39 -0700

    Update and rename Spike.java to Spikes.java

---
+++ Spikes.java
@@ -0,0 +1,13 @@
+public Spikes{
+    
+    Boolean touched;
+    public Spikes(int x, int y){
+        this.x = x;
+        this.y = y;
+        this.touched = False;
+        
+        }
+        
+    
+    
+    }
