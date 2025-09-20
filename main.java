c1593b202a75f274adb50ee542cfc17e915a4cd6
as41376-sy <as41376@pausd.us>
2025-09-20 15:46:49 -0700

    main.java

--- main.java
+++ main.java
@@ -3,9 +3,11 @@ public class Main {
     Platformer p = new Platformer();
     p.main();
     Platform1 = new Platform(50, 50, 100, 25);
-    Platform2 = new Platform(100, 200, 100, 25);
-    Platform3 = new Platform(150, 350, 100, 25);
-    Platform4 = new Platform(175, 500, 100, 25);
-    Platform5 = new Platform(75, 550, 75, 25);
+    Platform2 = new Platform(200, 100, 100, 25);
+    Platform3 = new Platform(350, 150, 100, 25);
+    Platform4 = new Platform(500, 175, 100, 25);
+    Platform5 = new Platform(550, 75, 25, 25);
+
+    Spikes1 = new Spikes(50, 25, 525, 25);
   }
 }
