cd458cd7c72548e08304e97950195e2ebe92cbf9
as41376-sy <as41376@pausd.us>
2025-09-20 15:40:00 -0700

    Update main.java

--- main.java
+++ main.java
@@ -2,5 +2,10 @@ public class Main {
   public static void main(String[] args) {
     Platformer p = new Platformer();
     p.main();
+    Platform1 = new Platform(50, 50, 100, 25);
+    Platform2 = new Platform(100, 200, 100, 25);
+    Platform3 = new Platform(150, 350, 100, 25);
+    Platform4 = new Platform(175, 500, 100, 25);
+    Platform5 = new Platform(75, 550, 75, 25);
   }
 }
