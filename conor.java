61c86570328757636c5c946f369b071922832310
flysandwichNOTFOUND <frankgao1145@gmail.com>
2025-09-22 22:11:24 -0700

    Update conor.java

--- conor.java
+++ conor.java
@@ -11,18 +11,36 @@ public class Platformer extends JPanel implements ActionListener, KeyListener {
     private final int gravity = 1;
     private final int moveSpeed = 5;
     private final int jumpStrength = 20;
-    public Platform p1 = new Platform(0,500,800,250);
-    public Platform p2 = new Platform(100,400,600,250);
-    public Platform[] platforms = new Platform[]{p1,p2};
-    Player player = new Player(50,300,50);
+    
+    private int level = 1;
+    private final int maxLevels = 3; //alter this if needed
+
+    
+    //=================================Alter this to move round the terrian===============================
+    private Platform[][] allLevels = {
+        // Level 1
+        { new Platform(0,500,800,250),
+        new Platform(100,400,600,250) },
+        
+        // Level 2
+        { 
+        new Platform(0,500,800,100),
+        new Platform(300,350,200,50),
+        new Platform(600,250,150,50)},
+        
+        // Level 3
+        { new Platform(0,550,800,50),
+        new Platform(150,450,150,50),
+        new Platform(400,350,150,50),
+        new Platform(650,250,100,50) } 
+    };
+    //=========================================================================================================
+    private Platform[] platforms = allLevels[0];
+    private Player player = new Player(50,300,50);
 
     public Platformer() {
         timer = new Timer(20, this);
         timer.start();
-        player.veloX = 0;
-        player.veloY = 0;
-        player.onGround = true;
-
         setFocusable(true);
         addKeyListener(this);
     }
@@ -31,14 +49,16 @@ public class Platformer extends JPanel implements ActionListener, KeyListener {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.CYAN);
-        g.fillRect(0, 0, getWidth(), getHeight()); // Background
+        g.fillRect(0,0,getWidth(),getHeight());
+
         g.setColor(Color.GREEN);
-        for(Platform p : platforms){
-            g.fillRect(p.x, p.y, p.l, p.w);
-        }
-         // Ground
+        for(Platform p : platforms) g.fillRect(p.x,p.y,p.l,p.w);
+
         g.setColor(Color.RED);
-        g.fillRect(player.x, player.y, player.s, player.s); // Player
+        g.fillRect(player.x, player.y, player.s, player.s);
+
+        g.setColor(Color.BLACK);
+        g.drawString("Level " + level, 20, 20);
     }
 
     @Override
@@ -50,17 +70,19 @@ public class Platformer extends JPanel implements ActionListener, KeyListener {
     private void update() {
         player.x += player.veloX;
         player.y += player.veloY;
+
+        boolean yCollision = false;
         for(Platform p : platforms){
-            if (player.touchingPlatform(p)) { // Ground collision
-                player.y = p.y - player.s;
-                player.veloY = 0;
-                player.onGround = true;
-            } else {
-                player.veloY += gravity;
-                player.onGround = false;
+            if(player.checkCollision(p)){
+                if(player.resolveCollision(p)) yCollision = true;
             }
         }
+        if(!yCollision){
+            player.veloY += gravity;
+            player.onGround = false;
+        }
 
+        if(player.x + player.s >= getWidth()) nextLevel();
     }
 
     @Override
@@ -93,6 +115,20 @@ public class Platformer extends JPanel implements ActionListener, KeyListener {
     @Override
     public void keyTyped(KeyEvent e) {}
 
+    private void nextLevel() {
+        if(level < maxLevels){
+            level++;
+            platforms = allLevels[level-1];
+            player.x = 50;
+            player.y = 300;
+            player.veloX = player.veloY = 0;
+        } else {
+            JOptionPane.showMessageDialog(this, "You beat the game!");
+            timer.stop();
+        }
+    }
+    //why dont we do this in the main file?
+    /*
     public static void main() {
         JFrame frame = new JFrame("Simple Platformer");
         //JLabel label = new JLabel("Level 1");
@@ -107,51 +143,4 @@ public class Platformer extends JPanel implements ActionListener, KeyListener {
         frame.setVisible(true);
     }
 }
-class Player{
-    int x,y;
-    int s;
-    int veloX = 0;
-    int veloY = 0;
-    boolean onGround = true;
-    Player(int x, int y, int s){
-        this.x = x;
-        this.y = y;
-        this.s = s;
-    }
-    boolean touchingPlatform(Platform p){
-        int playerLeft = x;
-        int playerRight = x+s;
-        int playerTop = y;
-        int playerBottom = y+s;
-
-        int platformLeft = p.x;
-        int platformRight = p.x+p.l;
-        int platformTop = p.y;
-        int platformBottom = p.y+p.w;
-        boolean xOverlap = (playerLeft<platformRight) && (playerRight>platformLeft);
-        boolean yOverlap = (playerTop<platformBottom) && (playerBottom>platformTop);
-        return xOverlap&&yOverlap;
-    }
-}
-class Platform{
-    int x, y;
-    int l, w;
-    Platform(int x, int y, int l, int w) {
-        this.x = x;
-        this.y = y;
-        this.l = l;
-        this.w = w;
-    }
-}
-class Spike{
-    int x, y;
-    int h, w;
-    Spike(int x, int y, int h, int w){
-        this.x = x;
-        this.y = y;
-        this.h = h;
-        this.w = w;
-
-    }
-
-}
+*/
