520d88bc229852a87ae36099256d89dc47b625f5
flysandwichNOTFOUND <frankgao1145@gmail.com>
2025-09-24 23:47:25 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -24,35 +24,24 @@ class Player {
 
         boolean xOverlap = (playerLeft < platformRight) && (playerRight > platformLeft);
         boolean yOverlap = (playerTop < platformBottom) && (playerBottom > platformTop);
-        /*
-        so what happended is, when the cube touches the wall in the first edition, it just moved the y-corrdinate up
-        becuase java thought it was, um, the ground. In order to fix that, we checks the overlap and seperated the x-coalition and y-coalition
-        ngl its hard
-        */
 
-
-        //please dont touch this part
-
+        // please don't touch this part
         if (xOverlap && yOverlap) {
-
             int overlapX = Math.min(playerRight - platformLeft, platformRight - playerLeft);
             int overlapY = Math.min(playerBottom - platformTop, platformBottom - playerTop);
 
             if (overlapX < overlapY) {
-                // X
-                if (playerRight - platformLeft < platformRight - playerLeft) {
-                    x = platformLeft - s; // left
-                } else {
-                    x = platformRight; // right
-                }
+                // X collision (wall)
+                if (playerRight - platformLeft < platformRight - playerLeft) x = platformLeft - s; // left
+                else x = platformRight; // right
                 veloX = 0;
             } else {
-                // Y
-                if (playerBottom - platformTop < platformBottom - playerTop) {
-                    y = platformTop - s; // top
+                // Y collision (floor/ceiling)
+                if (veloY > 0) { // only when falling
+                    y = platformTop - s; // land on top
                     onGround = true;
                 } else {
-                    y = platformBottom; // bottom
+                    y = platformBottom; // hitting ceiling
                 }
                 veloY = 0;
             }
@@ -60,6 +49,8 @@ class Player {
         }
         return false;
     }
+
+
     boolean checkCollision(Platform p) {
         int playerLeft = x, playerRight = x+s, playerTop = y, playerBottom = y+s;
         int platformLeft = p.x, platformRight = p.x+p.l, platformTop = p.y, platformBottom = p.y+p.w;
@@ -73,38 +64,37 @@ class Player {
         int overlapY = Math.min((y+s) - p.y, (p.y+p.w) - y);
 
         if (overlapX < overlapY) {
-            // Wall
+            // Horizontal collision (wall)
             if ((x+s) - p.x < (p.x+p.l) - x) x = p.x - s;
-            else x = p.x+p.l;
+            else x = p.x + p.l;
             veloX = 0;
             return false;
         } else {
-            // ground
+            // Vertical collision (floor/ceiling)
             if ((y+s) - p.y < (p.y+p.w) - y) {
                 y = p.y - s;
                 onGround = true;
             } else {
-                y = p.y+p.w;
+                y = p.y + p.w;
             }
             veloY = 0;
             return true;
         }
     }
 
-    boolean touchingEndPlatform(EndPlatform ep){
+    boolean touchingEndPlatform(Platform ep){
         int playerLeft = x;
         int playerRight = x+s;
         int playerTop = y;
         int playerBottom = y+s;
 
         int platformLeft = ep.x;
-        int platformRight = ep.x+ep.l;
+        int platformRight = ep.x + ep.l;
         int platformTop = ep.y;
-        int platformBottom = ep.y+ep.w;
+        int platformBottom = ep.y + ep.w;
 
         boolean xOverlap = (playerLeft < platformRight) && (playerRight > platformLeft);
         boolean yOverlap = (playerTop < platformBottom) && (playerBottom > platformTop);
-        return xOverlap&&yOverlap;
+        return xOverlap && yOverlap;
     }
-
 }
