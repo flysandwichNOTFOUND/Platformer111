8a236e0a082f00f817488216f36b892d1c3f1fe3
flysandwichNOTFOUND <frankgao1145@gmail.com>
2025-09-24 23:46:24 -0700

    Update CollisionDetector.java

--- CollisionDetector.java
+++ CollisionDetector.java
@@ -1,7 +1,9 @@
 class CollisionDetector{
-    static boolean collision(Player p, Spike s){
-        return false;
+    static boolean collision(Player p, Spikes s){
+        return p.x + p.s > s.x && p.x < s.x + s.w &&
+               p.y + p.s > s.y && p.y < s.y + s.h;
     }
+
     static boolean touchingEndPlatform(Player p, EndPlatform ep){
         int playerLeft = p.x;
         int playerRight = p.x+p.s;
@@ -15,8 +17,9 @@ class CollisionDetector{
 
         boolean xOverlap = (playerLeft < platformRight) && (playerRight > platformLeft);
         boolean yOverlap = (playerTop < platformBottom) && (playerBottom > platformTop);
-        return xOverlap&&yOverlap;
+        return xOverlap && yOverlap;
     }
+
     static boolean touchingPlatform(Player p, Platform pl){
         int playerLeft = p.x;
         int playerRight = p.x+p.s;
@@ -30,22 +33,20 @@ class CollisionDetector{
 
         boolean xOverlap = (playerLeft < platformRight) && (playerRight > platformLeft);
         boolean yOverlap = (playerTop < platformBottom) && (playerBottom > platformTop);
+
         /*
-        so what happended is, when the cube touches the wall in the first edition, it just moved the y-corrdinate up
-        becuase java thought it was, um, the ground. In order to fix that, we checks the overlap and seperated the x-coalition and y-coalition
-        ngl its hard
+        so what happened is, when the cube touches the wall in the first edition, it just moved the y-coordinate up
+        because Java thought it was the ground. To fix that, we check the overlap and separate X collision and Y collision
+        ngl it's hard
         */
 
-
-        //please dont touch this part
-
+        // please don't touch this part
         if (xOverlap && yOverlap) {
-
             int overlapX = Math.min(playerRight - platformLeft, platformRight - playerLeft);
             int overlapY = Math.min(playerBottom - platformTop, platformBottom - playerTop);
 
             if (overlapX < overlapY) {
-                // X
+                // X collision (wall)
                 if (playerRight - platformLeft < platformRight - playerLeft) {
                     p.x = platformLeft - p.s; // left
                 } else {
@@ -53,7 +54,7 @@ class CollisionDetector{
                 }
                 p.veloX = 0;
             } else {
-                // Y
+                // Y collision (floor/ceiling)
                 if (playerBottom - platformTop < platformBottom - playerTop) {
                     p.y = platformTop - p.s; // top
                     p.onGround = true;
@@ -66,6 +67,7 @@ class CollisionDetector{
         }
         return false;
     }
+
     static boolean checkCollision(Player p, Platform pl) {
         int playerLeft = p.x, playerRight = p.x+p.s, playerTop = p.y, playerBottom = p.y+p.s;
         int platformLeft = pl.x, platformRight = pl.x+pl.l, platformTop = pl.y, platformBottom = pl.y+pl.w;
@@ -79,18 +81,18 @@ class CollisionDetector{
         int overlapY = Math.min((p.y+p.s) - pl.y, (pl.y+pl.w) - p.y);
 
         if (overlapX < overlapY) {
-            // Wall
+            // Horizontal collision (wall)
             if ((p.x+p.s) - pl.x < (pl.x+pl.l) - p.x) p.x = pl.x - p.s;
-            else p.x = pl.x+pl.l;
+            else p.x = pl.x + pl.l;
             p.veloX = 0;
             return false;
         } else {
-            // ground
+            // Vertical collision (floor/ceiling)
             if ((p.y+p.s) - pl.y < (pl.y+pl.w) - p.y) {
                 p.y = pl.y - p.s;
                 p.onGround = true;
             } else {
-                p.y = pl.y+pl.w;
+                p.y = pl.y + pl.w;
             }
             p.veloY = 0;
             return true;
